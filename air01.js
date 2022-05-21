1911c3418d0e627d6837a631953963c16db7b5c5
Dylan Tettarasar <dtettarasar@gmail.com>
2022-05-21 11:26:20 +0200

    created air01

---
+++ air01.js
@@ -0,0 +1,28 @@
+// Split en fonction
+
+const argTester = () => {
+    const args = process.argv.slice(2);
+
+    if (args.length !== 2) {
+
+        console.log("Veuillez passer une seule chaîne de caractère en argument");
+        console.log("Exemple : node air01.js 'The Ace Of Spades' 'Of'");
+
+        return false;
+
+    }
+
+    return args;
+}
+
+const main = () => {
+
+    const str = argTester();
+
+    if (str) {
+        console.log(str);
+    }
+
+}
+
+main();
