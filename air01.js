3d1cd8a077498459969e74c8d99b290ec9db2ace
Dylan Tettarasar <dtettarasar@gmail.com>
2022-05-22 10:29:33 +0200

    air01 : update in argTester

--- air01.js
+++ air01.js
@@ -1,18 +1,24 @@
 // Split en fonction
 
 const argTester = () => {
+
     const args = process.argv.slice(2);
+
+    const strObj = {};
 
     if (args.length !== 2) {
 
-        console.log("Veuillez passer une seule chaîne de caractère en argument");
+        console.log("Veuillez passer deux chaînes de caractère en argument (la chaîne a découper puis le séparateur)");
         console.log("Exemple : node air01.js 'The Ace Of Spades' 'Of'");
 
         return false;
 
     }
 
-    return args;
+    strObj.strValue = args[0];
+    strObj.separator = args[1];
+
+    return strObj;
 }
 
 const mySplit = (str, separator) => {
