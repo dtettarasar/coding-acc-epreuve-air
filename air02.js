14174f887226f532b7e4e8daea69df99434a1919
Dylan Tettarasar <dtettarasar@gmail.com>
2022-05-28 17:36:19 +0200

    air02 : update in argTester

--- air02.js
+++ air02.js
@@ -4,13 +4,20 @@ const argTester = () => {
 
     const args = process.argv.slice(2);
 
+    const argObj = {
+        strArr: [],
+        separator: ""
+    }
+
     if (args.length < 3) {
         console.log("Veuillez passer au moins 3 chaînes de caractère en argument (les deux premières chaînes à joindre, puis en dernier le séparateur à utiliser pour la concaténation.)")
         console.log("Exemple: node air03.js 'system of a down' 'korn' 'slipknot' ', '");
         return false;
     }
 
-    console.log(args);
+    argObj.separator = args[args.length - 1];
+
+    console.log(argObj);
 
 }
 
