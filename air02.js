d0b64689464b4dc2f334b89ac1db0c5680ef0d53
Dylan Tettarasar <dtettarasar@gmail.com>
2022-05-28 17:39:53 +0200

    air02 : correct error message in argTester func

--- air02.js
+++ air02.js
@@ -11,11 +11,15 @@ const argTester = () => {
 
     if (args.length < 3) {
         console.log("Veuillez passer au moins 3 chaînes de caractère en argument (les deux premières chaînes à joindre, puis en dernier le séparateur à utiliser pour la concaténation.)")
-        console.log("Exemple: node air03.js 'system of a down' 'korn' 'slipknot' ', '");
+        console.log("Exemple: node air02.js 'system of a down' 'korn' 'slipknot' ', '");
         return false;
     }
 
     argObj.separator = args[args.length - 1];
+
+    for (let i = 0; i < args.length -1; i++) {
+        argObj.strArr.push(args[i]);
+    }
 
     console.log(argObj);
 
