10b97b3d439ef24dbccc46177af787f451cc4aa9
Dylan Tettarasar <dtettarasar@gmail.com>
2022-06-05 17:15:26 +0200

    air06 : added arrComparison and strInStr funcs

--- air06.js
+++ air06.js
@@ -1,5 +1,6 @@
 // Contrôle de pass sanitaire
 
+// Récupérer les arguments
 const argTester = () => {
 
     const arguments = process.argv.slice(2);
@@ -26,5 +27,52 @@ const argTester = () => {
     console.log(data);
 
 }
+
+// Comparer deux arrays
+const arrComparison = (arrOne, arrTwo) => {
+
+    if (arrOne.length === arrTwo.length) {
+
+        for (let i = 0; i < arrOne.length; i++) {
+
+            if (arrOne[i] !== arrTwo[i]) {
+                return false;
+            }
+
+        }
+
+        return true;
+    }
+
+}
+
+// Vérifie si la deuxième string est bien présente dans la première
+const strInStr = (fullStr, sampleStr) => {
+
+    const fullStrArr = fullStr.split('');
+    const sampleStrArr = sampleStr.split('');
+
+    for (let i = 0; i < fullStrArr.length;i++) {
+
+        if (fullStrArr[i] === sampleStrArr[0]) {
+
+            const fullStrExtract = [];
+
+            for (let j = 0; j < sampleStrArr.length; j++) {
+
+                fullStrExtract.push(fullStrArr[i + j]);
+
+            }
+
+            return arrComparison(fullStrExtract, sampleStrArr);
+
+        }
+
+    }
+
+    return false;
+
+}
+
 
 argTester();
