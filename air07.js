ae6f68a260461aaa8f3cb857ae1450c0f9020609
Dylan Tettarasar <dtettarasar@gmail.com>
2022-06-06 07:56:38 -0400

    air07 - argTester function  : added regex to test arguments

--- air07.js
+++ air07.js
@@ -2,9 +2,11 @@
 
 const argTester = () => {
 
-    const errorMsg = "Veuillez passer une série de nombre entier en argument\nExemple: node air07.js 1 3 4 2";
+    const errorMsg = "Veuillez passer une série de nombres entiers en argument (minimum 2 nombres)\nExemple: node air07.js 1 3 4 2";
 
     const argument = process.argv.slice(2);
+
+    const intPattern = /^-?[0-9]+$/;
 
     const data = {
         initialArr: [],
@@ -14,6 +16,15 @@ const argTester = () => {
     if (argument.length < 2) {
         console.log(errorMsg);
         return false;
+    }
+
+    for (let i = 0; i < argument.length; i++) {
+        
+        if (!intPattern.test(argument[i])) {
+            console.log(errorMsg);
+            return false;
+        }
+
     }
 
 }
