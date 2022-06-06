b68358235a0361fa264949a64cd1ab5a4411d2a9
Dylan Tettarasar <dtettarasar@gmail.com>
2022-06-06 08:04:13 -0400

    air07 : finish argTester

--- air07.js
+++ air07.js
@@ -1,5 +1,6 @@
 // Insertion dans un tableau trié
 
+// Récuperer argument
 const argTester = () => {
 
     const errorMsg = "Veuillez passer une série de nombres entiers en argument (minimum 2 nombres)\nExemple: node air07.js 1 3 4 2";
@@ -14,19 +15,33 @@ const argTester = () => {
     }
 
     if (argument.length < 2) {
+
         console.log(errorMsg);
         return false;
+
     }
 
     for (let i = 0; i < argument.length; i++) {
         
         if (!intPattern.test(argument[i])) {
+
             console.log(errorMsg);
             return false;
+
+        } else if (i < argument.length - 1) {
+
+            data.initialArr.push(parseInt(argument[i]));
+
+        } else {
+
+            data.elemToAdd = parseInt(argument[i]);
+
         }
 
     }
 
+    return data;
+
 }
 
-argTester();
+console.log(argTester());
