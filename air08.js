bab6f46078a5b0f12daccdfda595a8e3e3984e47
Dylan Tettarasar <dtettarasar@gmail.com>
2022-06-10 11:29:54 +0200

    air08 - argTester: remove one for loop

--- air08.js
+++ air08.js
@@ -4,7 +4,6 @@ const argTester = () => {
 
     const argument = process.argv.slice(2);
     const intPattern = /^-?[0-9]+$/;
-    const argArr = [];
     const errorMsg = "Veuillez passer en argument deux séries de nombres entiers, séparées par 'fusion'.\nExemple: node air07.js 10 20 30 fusion 15 25 35";
 
     let switchToSndArr = false;
@@ -21,23 +20,20 @@ const argTester = () => {
     for (let i = 0; i < argument.length; i++) {
 
         if (intPattern.test(argument[i]) || argument[i] === "fusion") {
-            argArr.push(argument[i]);
+
+            if (argument[i] === "fusion") {
+                switchToSndArr = true;
+            } else if (!switchToSndArr) {
+                data.arrOne.push(argument[i]);
+            } else {
+                data.arrTwo.push(argument[i]);
+            }
+
         } else {
             console.log(errorMsg);
             return false;
         }
 
-    }
-
-    for (let i = 0; i < argument.length; i++) {
-
-        if (argument[i] === "fusion") {
-            switchToSndArr = true;
-        } else if (!switchToSndArr) {
-            data.arrOne.push(argument[i]);
-        } else {
-            data.arrTwo.push(argument[i]);
-        }
     }
 
     return data;
