743b600ba5ad9ff59b06687885140607fd1bca3b
Dylan Tettarasar <dtettarasar@gmail.com>
2022-06-12 12:27:17 +0200

    air10 - argTester : handle file extension test

--- air10.js
+++ air10.js
@@ -2,16 +2,29 @@
 
 const argTester = () => {
 
-    const errorMsg = "Veuillez passer le nom d'un fichier en argument (format lisible : txt).\nExemple : node air10.js test.txt";
-    
+    const errorMsg = "Veuillez passer le nom d'un fichier en argument (formats lisibles : .txt, .md).\nExemple : node air10.js test.txt";
+    const fileFormat = [".txt", ".md"];  
     const argument = process.argv.slice(2);
+    let fileName = "";
 
     if (argument.length !== 1) {
         console.log(errorMsg);
         return false;
+    } else {
+        fileName = argument[0];
     }
 
-    console.log(argument);
+    for (let i = 0; i < fileFormat.length; i++) {
+
+        if (fileName.endsWith(fileFormat[i])) {
+            console.log(argument);
+            return true;
+        }
+
+    }
+
+    console.log(errorMsg);
+    return false;
 
 }
 
