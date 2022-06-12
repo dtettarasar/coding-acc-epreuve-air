96645fa9ce39cd20472984521c314c65efa4ccb1
Dylan Tettarasar <dtettarasar@gmail.com>
2022-06-12 12:50:18 +0200

    created air11

---
+++ air11.js
@@ -0,0 +1,14 @@
+// Afficher un pyramide
+
+const argTester = () => {
+
+}
+
+const buildPyramid = (char, int) => {
+    console.log(char);
+    console.log(int);
+}
+
+const main = () => {
+    
+}
