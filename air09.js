f8dfeb029e88340cb540c27cc7bd1c4b1e67cab6
Dylan Tettarasar <dtettarasar@gmail.com>
2022-06-12 11:35:02 +0200

    air09: finish argTester func

--- air09.js
+++ air09.js
@@ -2,12 +2,30 @@
 
 const argTester = () => {
 
+    const arguments = process.argv.slice(2);
+
+    if (arguments.length < 2) {
+        console.log("Veuillez passer au moins deux arguments.");
+        console.log("Exemple : node air09.js Rust in Peace");
+        return false;
+    }
+
+    return arguments
+
 }
 
 const rotateArr = (array) = {
-    
+
 }
 
 const main = () => {
 
+    const arguments = argTester();
+
+    if (arguments) {
+        console.log(arguments);
+    }
+
 }
+
+main();
