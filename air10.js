55df5e7a5ff000590597b284f26bbaebf906e5cb
Dylan Tettarasar <dtettarasar@gmail.com>
2022-06-12 12:16:43 +0200

    air10 : update in argTester

--- air10.js
+++ air10.js
@@ -1,1 +1,18 @@
 // Afficher le contenu
+
+const argTester = () => {
+
+    const errorMsg = "Veuillez passer le nom d'un fichier en argument (format lisible : txt).\nExemple : node air10.js test.txt";
+    
+    const argument = process.argv.slice(2);
+
+    if (argument.length !== 1) {
+        console.log(errorMsg);
+        return false;
+    }
+
+    console.log(argument);
+
+}
+
+argTester();
