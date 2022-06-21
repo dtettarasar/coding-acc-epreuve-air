1d4fab9c38947217af55e1f0ca076fae07196447
Dylan Tettarasar <dtettarasar@gmail.com>
2022-06-21 08:12:40 -0400

    air00 : update comment

--- air00.mjs
+++ air00.mjs
@@ -64,7 +64,7 @@ const main = () => {
 const args = process.argv;
 const arr = args[1].split('/');
 
-// Execute main
+// Execute main if we launch the script directly in terminal
 if (arr[arr.length - 1] === "air00.mjs") {
     main();
 }
