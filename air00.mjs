11806dd9ef52c8205cc55ffee0420e978baf484c
Dylan Tettarasar <dtettarasar@gmail.com>
2022-06-23 18:00:23 +0200

    air00 : update error message

--- air00.mjs
+++ air00.mjs
@@ -6,7 +6,7 @@ const argTester = () => {
     if (args.length !== 1) {
 
         console.log("Veuillez passer une seule chaîne de caractère en argument");
-        console.log("Exemple : node air00.js 'Fear Of The Dark'");
+        console.log("Exemple : node air00.mjs 'Fear Of The Dark'");
 
         return false;
 
