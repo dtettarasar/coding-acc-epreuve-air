1c03e0dea6a237715d5fd093047d5688d67b5135
Dylan Tettarasar <dtettarasar@gmail.com>
2022-06-24 11:48:48 -0400

    air13 : update in import air01 funcs

--- air13.mjs
+++ air13.mjs
@@ -3,11 +3,13 @@
 // Import des fonctions à tester
 
 import {mySplit as air00Split} from "./air00.mjs";
-
-import {arrComparison, findSepIndexes, getStrExtract, mySplit as air01Split} from "./air01.mjs";
+import {mySplit as air01Split} from "./air01.mjs";
 
 // Test air00
+console.log("test air00");
 console.log(air00Split("Fear Of The Dark"));
+console.log("--------");
 
 // Test air01
+console.log("test air01");
 console.log(air01Split('The Ace Of Spades', 'Of'));
