44c4c531b92017a987f381d6c7bac7dcdb265954
Dylan Tettarasar <dtettarasar@gmail.com>
2022-07-20 18:02:23 +0200

    air12 : change file extension

--- air12.js
+++ air12.js
@@ -86,13 +86,4 @@ const main = () => {
 
 }
 
-//Get file name
-const args = process.argv;
-const arr = args[1].split('/');
-
-// Execute main if we launch the script directly in terminal
-if (arr[arr.length - 1] === "air12.mjs") {
-    main();
-}
-
-export {quickSortProcess};
+main();
