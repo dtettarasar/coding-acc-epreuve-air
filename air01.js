3df022a01174d75db2829111627d816d30247e93
Dylan Tettarasar <dtettarasar@gmail.com>
2022-07-20 17:36:42 +0200

    air01 : change file extension

--- air01.js
+++ air01.js
@@ -146,13 +146,4 @@ const main = () => {
 
 }
 
-//Get file name
-const args = process.argv;
-const arr = args[1].split('/');
-
-// Execute main if we launch the script directly in terminal
-if (arr[arr.length - 1] === "air01.mjs") {
-    main();
-}
-
-export {mySplit};
+main();
