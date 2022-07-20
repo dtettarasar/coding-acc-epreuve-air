35995e19bd8a6aeb9d206704d27df00238a711e0
Dylan Tettarasar <dtettarasar@gmail.com>
2022-07-20 17:43:15 +0200

    air04 : change file extension

--- air04.js
+++ air04.js
@@ -42,13 +42,4 @@ const main = () => {
 
 }
 
-//Get file name
-const args = process.argv;
-const arr = args[1].split('/');
-
-// Execute main if we launch the script directly in terminal
-if (arr[arr.length - 1] === "air04.mjs") {
-    main();
-}
-
-export {removeDuplicateChar};
+main();
