beeedec770781a1e15911749fbefd7967c275550
Dylan Tettarasar <dtettarasar@gmail.com>
2022-07-20 17:49:03 +0200

    air07 : change file extension

--- air07.js
+++ air07.js
@@ -91,13 +91,4 @@ const main = () => {
 
 }
 
-//Get file name
-const args = process.argv;
-const arr = args[1].split('/');
-
-// Execute main if we launch the script directly in terminal
-if (arr[arr.length - 1] === "air07.mjs") {
-    main();
-}
-
-export {sortedInsert};
+main();
