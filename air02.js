9bf19a8e5cbba0cd07f2bbe42ccd0b9f98409258
Dylan Tettarasar <dtettarasar@gmail.com>
2022-07-20 17:39:53 +0200

    air02 : change file extension

--- air02.js
+++ air02.js
@@ -54,13 +54,4 @@ const main = () => {
 
 }
 
-//Get file name
-const args = process.argv;
-const arr = args[1].split('/');
-
-// Execute main if we launch the script directly in terminal
-if (arr[arr.length - 1] === "air02.mjs") {
-    main();
-}
-
-export {concatenation};
+main();
