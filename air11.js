fe2cc2581a4d93476e0ceae89e86cc46e2528568
Dylan Tettarasar <dtettarasar@gmail.com>
2022-07-20 18:01:18 +0200

    air11 : change file extension

--- air11.js
+++ air11.js
@@ -69,13 +69,4 @@ const main = () => {
 
 }
 
-//Get file name
-const args = process.argv;
-const arr = args[1].split('/');
-
-// Execute main if we launch the script directly in terminal
-if (arr[arr.length - 1] === "air11.mjs") {
-    main();
-}
-
-export {buildPyramid};
+main();
