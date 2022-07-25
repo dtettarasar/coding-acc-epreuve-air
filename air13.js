851c10d045f896f7b03b530528e764990197a804
Dylan Tettarasar <dtettarasar@gmail.com>
2022-07-25 12:01:58 -0400

    air13 : created getScriptToTest func

--- air13.js
+++ air13.js
@@ -1,18 +1,18 @@
 // Meta exercice
 
-const test00 = require('./air00.js')();
-const test01 = require('./air01.js')();
-const test02 = require('./air02.js')();
-const test03 = require('./air03.js')();
-const test04 = require('./air04.js')();
-const test05 = require('./air05.js')();
-const test06 = require('./air06.js')();
-const test07 = require('./air07.js')();
-const test08 = require('./air08.js')();
-const test09 = require('./air09.js')();
-const test10 = require('./air10.js')();
-const test11 = require('./air11.js')();
-const test12 = require('./air12.js')();
+const getScriptToTest = (fileName) => {
+
+    const filePath = './' + fileName + '.js';
+    let fileToTest = null;
+
+    try {
+        testResult = require(filePath)();
+        testStarter(fileName, testResult);
+    } catch {
+        console.log("\x1b[31m", 'missing file: ' + filePath);
+    }
+
+} 
 
 let totalTest = 0;
 let totalSuccesses = 0;
@@ -80,17 +80,8 @@ const getTestResult = (argObj) => {
 
 }
 
-testStarter("air00", test00);
-testStarter("air01", test01);
-testStarter("air02", test02);
-testStarter("air03", test03);
-testStarter("air04", test04);
-testStarter("air05", test05);
-testStarter("air06", test06);
-testStarter("air07", test07);
-testStarter("air08", test08);
-testStarter("air09", test09);
-testStarter("air10", test10);
-testStarter("air11", test11);
-testStarter("air12", test12);
+getScriptToTest("air00");
+getScriptToTest("air01");
+getScriptToTest("air02");
+getScriptToTest("air15");
 console.log( "\x1b[37m", "Total success: ("+ totalSuccesses +"/"+ totalTest +")");
